25787e1908d1b2327cb78acd35c8357c0a5b8fa7
Alexander Hafstad <alexander@hafstad.dk>
2017-10-23 19:57:27 +0200

    feat(gameboard): added gameboard component

---
+++ src/app/gameboard/gameboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'itu-gameboard',
+  templateUrl: './gameboard.component.html',
+  styleUrls: ['./gameboard.component.scss']
+})
+export class GameboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
