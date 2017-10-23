5a42ed182672614e57221dd2107ee5601d3d0e34
Alexander Hafstad <alexander@hafstad.dk>
2017-10-23 21:10:40 +0200

    feat(card): added input for the card component

--- src/app/gameboard/card/card.component.ts
+++ src/app/gameboard/card/card.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Card } from '../../models/card';
 
 @Component({
   selector: 'itu-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent { }
+export class CardComponent {
+  private _card: Card;
+
+  get card(): Card {
+    return this._card;
+  }
+
+  @Input()
+  set card(card: Card) {
+    this._card = card;
+  }
+}
